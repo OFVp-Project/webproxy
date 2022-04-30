3ce42685972e711c304e99234294016dfe57bf9d
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-04-30 21:45:08 +0000

    Update server and handler

--- src/handler.ts
+++ src/handler.ts
@@ -40,9 +40,23 @@ export class connectionHandler {
   private logLevel: "LOG1"|"DEBUG1"|"NONE" = "LOG1";
   private httpCode: number = 200;
   private httpMessage: string = "OK";
-  private httpVersion: "1.0"|"1.1" = "1.0";
-  private clientIpre = "";
-  constructor (client: net.Socket, sshHost: string, sshPort: number, httpCode: number, httpMessage: string, httpVersion: "1.0"|"1.1", logLevel: "LOG1"|"DEBUG1"|"NONE") {
+  private httpVersion: string = "1.0";
+  public clientIpre = "";
+  private allowReplaceHostByHeader: true|false = false;
+  private connectionPayload: {raw: string, method: string, httpVersion: string, path: string, header: {[key: string]: string}, second?: {method: string, httpVersion: string, path: string, header: {[key: string]: string}}} = {
+    second: {
+      method: "",
+      httpVersion: "",
+      path: "",
+      header: {},
+    },
+    method: "",
+    httpVersion: "",
+    path: "",
+    header: {},
+    raw: "",
+  }
+  constructor (client: net.Socket, sshHost: string, sshPort: number, httpCode: number, httpMessage: string, httpVersion: string, logLevel: "LOG1"|"DEBUG1"|"NONE", allowReplaceHostByHeader: true|false) {
     this.client = client
     this.sshHost = sshHost
     this.sshPort = sshPort
@@ -50,14 +64,15 @@ export class connectionHandler {
     this.httpCode = httpCode
     this.httpMessage = httpMessage
     this.httpVersion = httpVersion
-    this.clientIpre = `${this.client.remoteAddress}:${this.client.remotePort}`
-    this.client.once("close", () => {
-      if (this.logLevel !== "NONE") console.log("%s wsSSH: Client disconnected", this.clientIpre);
-      this.closed = true;
-    });
+    this.clientIpre = `${this.client.remoteAddress}:${this.client.remotePort}`;
+    this.allowReplaceHostByHeader = allowReplaceHostByHeader
+    
+    // Connection log
+    if (this.logLevel !== "NONE") console.log("%s wsSSH (Client): Connected", this.clientIpre);
+    this.client.once("close", () => {if (this.logLevel !== "NONE") console.log("%s wsSSH (Client): disconnected", this.clientIpre)});
     this.client.once("error", err => {
       if (!this.closed) {
-        if (this.logLevel !== "NONE") console.log("%s wsSSH: Client error: %s", this.clientIpre, err.message);
+        if (this.logLevel !== "NONE") console.log("%s wsSSH (Client): error: %s", this.clientIpre, err.message);
         return
       }
       this.closed = true;
@@ -82,41 +97,20 @@ export class connectionHandler {
     }
     return;
   }
-  
-  private findHeader(data: string, header: string): string | undefined {
-    for (const line of data.split("\r\n")) {
-      if (line.startsWith(header)) {
-        return line.replace(header+":", "").trim();
-      }
-    }
-    return undefined;
-  }
 
-  private method = "GET";
-  private async connect_target(host: string) {
-    let port: number = this.sshPort;
-    if (host === undefined) host = `${this.sshHost}:${this.sshPort}`;
-    const exits = host.includes(":");
-    if (exits) {
-      const [hostname, portI] = host.split(":");
-      port = parseInt(portI);
-      host = hostname;
-      if (isNaN(port)) {
-        if (this.logLevel !== "NONE") console.log("%s wsSSH (SSH): Invalid port: %s", this.clientIpre, portI);
-        return;
-      }
-    } else {
-      if (this.method === "CONNECT") port = 443;
-      else port = 80;
-    }
-    if (/undefined/.test(host)) host = this.sshHost;
-    this.target = net.createConnection({port: port, host: host});
+  /** Set target (SSH) connection */
+  private connect_target() {
+    this.target = net.createConnection({port: this.sshPort, host: this.sshHost});
     this.closed = false;
     this.target.once("connect", () => {
-      if (this.logLevel !== "NONE") console.log("%s wsSSH (SSH): Connected to %s:%d", this.clientIpre, host, port);
+      if (this.logLevel !== "NONE") console.log("%s wsSSH (SSH): Connected to %s:%d", this.clientIpre, this.sshHost, this.sshPort);
     });
     this.target.once("error", (err) => {
-      if (this.logLevel !== "NONE") console.log("%s wsSSH (SSH): Error connecting to %s:%d: %s", this.clientIpre, host, port, err.message);
+      if (this.logLevel !== "NONE") console.log("%s wsSSH (SSH): Error connecting to %s:%d: %s", this.clientIpre, this.sshHost, this.sshPort, err.message);
+      this.closeClient();
+    });
+    this.target.once("close", () => {
+      if (this.logLevel !== "NONE") console.log("%s wsSSH (SSH): Disconnected from %s:%d", this.clientIpre, this.sshHost, this.sshPort);
       this.closeClient();
     });
   }
@@ -125,8 +119,16 @@ export class connectionHandler {
    * After the client and target are connected, this function will transmit data between them
    */
   private async ClientConnectAndTransmit() {
-    this.client.pipe(this.target);
-    this.target.pipe(this.client);
+    this.client.on("data", data => {
+      if (this.closed) return;
+      this.target.write(data);
+    });
+    
+    this.target.on("data", data => {
+      if (this.closed) return;
+      this.client.write(data);
+    })
+
     await new Promise((resolve) => {
       this.client.once("close", resolve);
       this.target.once("close", resolve);
@@ -139,11 +141,9 @@ export class connectionHandler {
     this.client.write(`${MessageToSend}\r\n\r\n`);
   }
 
-  private async ConnectMethod(hostPort?: string) {
-    if (hostPort === undefined) hostPort = `${this.sshHost}:${this.sshPort}`;
-    else if (/undefined/.test(hostPort)) hostPort = `${this.sshHost}:${this.sshPort}`;
+  private async ConnectMethod() {
     this.sendMenssage();
-    this.connect_target(hostPort);
+    this.connect_target();
     await slectWait([this.client], [this.target]);
     this.ClientConnectAndTransmit();
   }
@@ -154,26 +154,57 @@ export class connectionHandler {
         resolve(data.toString());
       });
     });
-    if (this.logLevel === "DEBUG1") console.log("%s wsSSH: Received data: %s", this.clientIpre, data);
-    let hostPort = this.findHeader(data, "X-Real-Host")
-    if (!hostPort) hostPort = this.sshHost+":"+this.sshPort;
+    this.connectionPayload.raw = data;
+    // Parse init Payload
+    for (const line of data.replace(/\r/g, "").split("\n")) {
+      if (/^GET|POST|CONNECT|HEAD|PUT|DELETE|OPTIONS|TRACE|PATCH|PROPFIND|PROPPATCH|MKCOL|COPY|MOVE|LOCK|UNLOCK|VERSION-CONTROL/.test(line)) {
+        const dataPay = line.match(/^(.*)\s+(.*)\s+HTTP\/(.*)/);
+        if (dataPay) {
+          if (!this.connectionPayload.method) {
+            this.connectionPayload.method = dataPay[1];
+            this.connectionPayload.path = dataPay[2];
+            this.connectionPayload.httpVersion = dataPay[3].trim();
+          } else {
+            this.connectionPayload.second.method = dataPay[1];
+            this.connectionPayload.second.path = dataPay[2];
+            this.connectionPayload.second.httpVersion = dataPay[3].trim();
+          }
+        }
+      } else if (line.includes(":")) {
+        const [key, value] = line.split(":");
+        if (!this.connectionPayload.second.method) this.connectionPayload.header[key.trim()] = value.trim();
+        else this.connectionPayload.second.header[key.trim()] = value.trim();
+      }
+    };
+    if (this.logLevel === "DEBUG1") console.log("%s wsSSH: parsed headers:\n%o", this.clientIpre, this.connectionPayload);
+    let hostPort = this.connectionPayload.header["X-Real-Host"]||this.connectionPayload.second?.header["X-Real-Host"]||"";
+    if (hostPort.includes(":")) {
+      this.sshHost = hostPort.split(":")[0];
+      this.sshPort = parseInt(hostPort.split(":")[1]);
+    }
     
-    // const UserAgent = this.findHeader(data, "User-Agent")||"";
-    // if (/curl|wget/.test(UserAgent.toLowerCase())) {
-    //   this.client.write(`HTTP/1.1 200 OK\r\n\r\n`);
-    //   this.client.end("Agent not allowed");
-    //   return;
-    // }
+    if (/<\?=md5("phpunit")\?>/.test(data)) {
+      this.client.write(`HTTP/1.1 60000 CVE-2017-9841\r\n\r\n`);
+      this.client.end("It's trying to use CVE-2017-9841 but unable to use PHPUnit because PHPUnit is not compatible with Nodejs, By OFVp Project, https://github.com/OFVp-Project/webproxy/tree/src/handler.ts#L1630\n");
+      return;
+    }
 
-    let split = this.findHeader(data, "X-Split")
-    if (!split) {}
-    
-    if (hostPort !== "") {
-      let PASS = ""
-      let passwd = this.findHeader(data, "X-Pass")
-      if (PASS.length !== 0 && passwd === PASS) this.ConnectMethod(hostPort)
-      else if (PASS.length !== 0 && passwd !== PASS) this.client.write('HTTP/1.1 400 WrongPass!\r\n\r\n')
-      this.ConnectMethod(hostPort)
-    } else this.client.write('HTTP/1.1 400 NoXRealHost!\r\n\r\n')
+    if (this.connectionPayload.second?.method === "CONNECT") {
+      if (this.allowReplaceHostByHeader) {
+        const [host, port] = this.connectionPayload.second?.path.split(":");
+        if (!!host) this.sshHost = host;
+        if (!!port)this.sshPort = parseInt(port)||80;
+        console.log("%s wsSSH (Client): Replaced host to %s:%d", this.clientIpre, this.sshHost, this.sshPort);
+      }
+      this.ConnectMethod();
+    } else if (this.connectionPayload.method === "CONNECT") {
+      if (this.allowReplaceHostByHeader) {
+        const [host, port] = this.connectionPayload.path.split(":");
+        if (!!host) this.sshHost = host;
+        if (!!port)this.sshPort = parseInt(port)||80;
+        console.log("%s wsSSH (Client): Replaced host to %s:%d", this.clientIpre, this.sshHost, this.sshPort);
+      }
+      this.ConnectMethod();
+    } else this.ConnectMethod()
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+#!/usr/bin/env node
 import { createServer } from "net";
 import { randomUUID } from "crypto";
 import yargs from "yargs";
 import { connectionHandler } from "./handler";
 
-const cmdOptions = yargs(process.argv.slice(2)).alias("h", "help").option("port", {type: "number",
+const cmdOptions = yargs(process.argv.slice(2)).alias("h", "help").alias("v", "version").wrap(yargs.terminalWidth()).option("port", {type: "number",
   default: 80,
   alias: "p",
-  description: "Port to listen on"
+  description: "Port to listen server"
 }).option("ssh", {
   type: "string",
   default: "0.0.0.0:22",
@@ -14,12 +15,12 @@ const cmdOptions = yargs(process.argv.slice(2)).alias("h", "help").option("port"
   description: "SSH host and port"
 }).option("code", {
   type: "number",
-  default: 101,
+  default: 200,
   alias: "c",
   description: "Proxy HTTP status code"
 }).option("message", {
   type: "string",
-  default: "<font color=\"null\">By ofvp project</font>",
+  default: "<font color=\"green\">By OFVp Project</font>",
   alias: "m",
   description: "Proxy HTTP status message"
 }).option("httpVersion", {
@@ -33,24 +34,50 @@ const cmdOptions = yargs(process.argv.slice(2)).alias("h", "help").option("port"
   alias: "l",
   description: "Log level to show in console",
   default: "LOG1",
-  choices: ["LOG1", "DEBUG1", "NONE"]
+  choices: [
+    "NONE",   "none",  "0",
+    "LOG1",   "log",   "1",
+    "DEBUG1", "debug", "2"
+  ]
+}).option("allowReplaceHost", {
+  type: "boolean",
+  default: false,
+  alias: "r",
+  description: "Allow to replace host if includes CONNECT in header"
 }).help().parseSync();
 
-const portListen = cmdOptions.port, httpCode = cmdOptions.code, httpMessage = cmdOptions.message, httpVersion = cmdOptions.httpVersion as "1.1"|"1.0", logLevel = cmdOptions.loglevel.toUpperCase() as "LOG1"|"DEBUG1"|"NONE", ssh = {host: cmdOptions.ssh.split(":")[0], port: parseInt(cmdOptions.ssh.split(":")[1])};
-console.log("wsSSH: Starting web proxy on port %d", portListen)
-console.log("wsSSH: SSH host: %s", cmdOptions.ssh)
-console.log("wsSSH: HTTP status code: %d", httpCode)
-console.log("wsSSH: HTTP status message: %s", httpMessage)
-console.log("wsSSH: HTTP version: HTTP/%s", httpVersion)
-console.log("wsSSH: log Leve %s", logLevel)
-console.log("wsSSH: Starting web proxy...\n****** LOG ******\n")
+const
+portListen = cmdOptions.port,
+httpCode = cmdOptions.code,
+httpMessage = cmdOptions.message,
+httpVersion = cmdOptions.httpVersion,
+ssh = {host: cmdOptions.ssh.split(":")[0], port: parseInt(cmdOptions.ssh.split(":")[1])},
+allowReplaceHost = cmdOptions.allowReplaceHost;
+let logLevel: "LOG1"|"DEBUG1"|"NONE" = "LOG1";
+if (cmdOptions.loglevel.toUpperCase() === "LOG1"||cmdOptions.loglevel.toUpperCase() === "LOG"||cmdOptions.loglevel.toUpperCase() === "1") logLevel = "LOG1";
+else if (cmdOptions.loglevel.toUpperCase() === "DEBUG1"||cmdOptions.loglevel.toUpperCase() === "DEBUG"||cmdOptions.loglevel.toUpperCase() === "2") logLevel = "DEBUG1";
+else if (cmdOptions.loglevel.toUpperCase() === "NONE"||cmdOptions.loglevel.toUpperCase() === "0") logLevel = "NONE";
+else throw new Error("Unknown log level");
+
+// Show options selected
+console.log("wsSSH: log Leve %s", logLevel);
+console.log("wsSSH: Default host connect: %s", cmdOptions.ssh);
+console.log("wsSSH: HTTP status code: %d", httpCode);
+console.log("wsSSH: HTTP status message: %s", httpMessage);
+console.log("wsSSH: HTTP version: HTTP/%s", httpVersion);
+console.log("wsSSH:", allowReplaceHost?"allow replace host by header":"not allow replace host by header");
+console.log("wsSSH: Listen on %d", portListen);
+console.log("wsSSH: Starting web proxy...")
 
 // Sessions and stop connections on sigint
 const Sessions: {[key: string]: connectionHandler} = {};
 process.on("SIGINT", async () => {
   console.log("\nClosing all connections...");
   for (const session of Object.keys(Sessions)) {
-    Sessions[session].closeClient("Server is shutting down", 503);
+    if (!Sessions[session].closed) {
+      Sessions[session].closeClient("Server is shutting down", 503);
+      console.log("Closed connection: %s", Sessions[session].clientIpre);
+    }
   }
   console.log("Closing server...");
   process.exit(0);
@@ -59,20 +86,16 @@ process.on("SIGINT", async () => {
 // Create TCP socket server
 const serverListen = createServer();
 serverListen.setMaxListeners(0);
-serverListen.listen(portListen, "0.0.0.0", () => console.log("wsSSH: Starting web proxy on port %d", portListen));
+serverListen.listen(portListen, "0.0.0.0", () => console.log("wsSSH: web proxy listen on port %d\n****** LOG ******\n", portListen));
 
 // Handle new connections
 serverListen.on("connection", (connection) => {
-  if (logLevel !== "NONE") console.log("wsSSH: Client connected: %s", connection.remoteAddress+":"+connection.remotePort);
   connection.allowHalfOpen = true;
   connection.setNoDelay(true);
   connection.setKeepAlive(true);
-  const Connection = new connectionHandler(connection, ssh.host, ssh.port, httpCode, httpMessage, httpVersion, logLevel);
+  const Connection = new connectionHandler(connection, ssh.host, ssh.port, httpCode, httpMessage, httpVersion, logLevel, allowReplaceHost);
   const id = randomUUID();
   Sessions[id] = Connection;
   Connection.main();
-  connection.once("close", () => {
-    if (!!Sessions[id]) delete Sessions[id];
-    if (logLevel !== "NONE") console.log("wsSSH: Client disconnected: %s", connection.remoteAddress+":"+connection.remotePort);
-  });
+  connection.once("close", () => {if (!!Sessions[id]) delete Sessions[id];});
 });
